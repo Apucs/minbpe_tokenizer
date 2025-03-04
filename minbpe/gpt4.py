3f2b58c6daaf91abad1be376f144103b68154fb4
Apucs <apu3660@gmail.com>
2025-03-05 03:09:04 +0600

    experiments with gpt4 tokenizer

---
+++ minbpe/gpt4.py
@@ -0,0 +1,66 @@
+import tiktoken
+from .regex import RegexTokenizer
+
+def bpe(mergeable_ranks, token, max_rank):
+    # helper function used in get_gpt4_merges() to reconstruct the merge forest
+    parts = [bytes([b]) for b in token]
+    while True:
+        min_idx = None
+        min_rank = None
+        for i, pair in enumerate(zip(parts[:-1], parts[1:])):
+            rank = mergeable_ranks.get(pair[0] + pair[1])
+            if rank is not None and (min_rank is None or rank < min_rank):
+                min_idx = i
+                min_rank = rank
+        if min_rank is None or (max_rank is not None and min_rank >= max_rank):
+            break
+        assert min_idx is not None
+        parts = parts[:min_idx] + [parts[min_idx] + parts[min_idx + 1]] + parts[min_idx + 2:]
+    return parts
+
+
+def recover_merges(mergeable_ranks):
+    # the `merges` are already the byte sequences in their merged state.
+    # so we have to recover the original pairings. We can do this by doing
+    # a small BPE training run on all the tokens, in their order.
+    # also see https://github.com/openai/tiktoken/issues/60
+    # also see https://github.com/karpathy/minbpe/issues/11#issuecomment-1950805306
+    merges = {}
+    for token, rank in mergeable_ranks.items():
+        if len(token) == 1:
+            continue # skip raw bytes
+        pair = tuple(bpe(mergeable_ranks, token, max_rank=rank))
+        assert len(pair) == 2
+        # recover the integer ranks of the pair
+        ix0 = mergeable_ranks[pair[0]]
+        ix1 = mergeable_ranks[pair[1]]
+        merges[(ix0, ix1)] = rank
+
+    return merges
+
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+GPT4_SPECIAL_TOKENS = {
+    '<|endoftext|>': 100257,
+    '<|fim_prefix|>': 100258,
+    '<|fim_middle|>': 100259,
+    '<|fim_suffix|>': 100260,
+    '<|endofprompt|>': 100276
+}
+
+
+enc = tiktoken.get_encoding("cl100k_base")
+mergeable_ranks = enc._mergeable_ranks
+
+print(dict(list(mergeable_ranks.items())[:5]))
+merges = recover_merges(mergeable_ranks)
+print(dict(list(merges.items())[:5]))
+
+vocab = {idx: bytes([idx]) for idx in range(256)}
+for (p0, p1), idx in merges.items():
+    vocab[idx] = vocab[p0] + vocab[p1]
+
+print(dict(list(vocab.items())[:5]), dict(list(vocab.items())[-5:]),"\n")
+
+byte_shuffle = {i: mergeable_ranks[bytes([i])] for i in range(256)}
+print(byte_shuffle)
+inverse_byte_shuffle = {v: k for k, v in byte_shuffle.items()}
--- test.py
+++ test.py
@@ -3,24 +3,33 @@ from minbpe import BasicTokenizer, RegexTokenizer
 with open("tests/taylorswift.txt", "r") as file:
     text = file.read()
 
-basic_tokenizer = BasicTokenizer()
-basic_tokenizer.fit(text, vocab_size=2000, verbose=False)  #training the vocab
+# basic_tokenizer = BasicTokenizer()
+# basic_tokenizer.fit(text, vocab_size=2000, verbose=False)  #training the vocab
 
-#testing the trained tokenizer
+# #testing the trained tokenizer
 
 # valtext = "Many common characters, including numerals123456789, punctuation, and other symbols, are unified within the standard and are not treated as specific to any given writing system. Unicode encodes thousands of emoji, with the continued development thereof conducted by the Consortium as a part of the standard.[4] Moreover, the widespread adoption of Unicode was in large part responsible for the initial popularization of emoji outside of Japan. Unicode is ultimately capable of encoding more than 1.1 million characters."
 valtext = "hello123!!!?     (안녕하세요!) 😉"
 print(valtext)
 
-encoded = basic_tokenizer.encoder(valtext)
-print("length of encoded ids with basic tokenizer:", len(encoded), "encoded:",encoded)
-decoded = basic_tokenizer.decoder(encoded)
-# print(encoded)
-# print(decoded)
-print(valtext == decoded)
+# encoded = basic_tokenizer.encoder(valtext)
+# print("length of encoded ids with basic tokenizer:", len(encoded), "encoded:",encoded)
+# decoded = basic_tokenizer.decoder(encoded)
+# # print(encoded)
+# # print(decoded)
+# print(valtext == decoded)
 
 regex_tokenizer = RegexTokenizer()
 regex_tokenizer.fit(text, vocab_size=5000, verbose=False)  #training the vocab
+regex_tokenizer.save("regex")
+
+regex_tokenizer_2 = RegexTokenizer()
+regex_tokenizer_2.load("regex.model")
+print(regex_tokenizer_2.pattern)
+print(regex_tokenizer_2.special_tokens)
+print(regex_tokenizer_2.merges)
+print(regex_tokenizer_2.vocab)
+
 encoded = regex_tokenizer.encoder(valtext)
 print("length of encoded ids with regex tokenizer:", len(encoded), "encoded:",encoded)
 decoded = regex_tokenizer.decoder(encoded)
